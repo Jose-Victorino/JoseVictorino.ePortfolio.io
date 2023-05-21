56e7e9764786f38bd9c41b634c3fe8eed596551a
JoseVictorino <120122644+Jose-Victorino@users.noreply.github.com>
2023-05-21 19:15:55 +0800

    Update global-script.js

--- Scripts/global-script.js
+++ Scripts/global-script.js
@@ -95,40 +95,40 @@ window.addEventListener('scroll', () => {
 
 
 // loading screen
-// const body = document.getElementsByTagName('body');
-// const preloader = document.getElementById('load-Cont');
-// const mediaQuery1 = window.matchMedia('(max-width: 1200px)');
-// const mediaQuery2 = window.matchMedia('(max-width: 950px)');
-// const mediaQuery3 = window.matchMedia('(max-width: 740px)');
-// const mediaQuery4 = window.matchMedia('(max-width: 560px)');
+const body = document.getElementsByTagName('body');
+const preloader = document.getElementById('load-Cont');
+const mediaQuery1 = window.matchMedia('(max-width: 1200px)');
+const mediaQuery2 = window.matchMedia('(max-width: 950px)');
+const mediaQuery3 = window.matchMedia('(max-width: 740px)');
+const mediaQuery4 = window.matchMedia('(max-width: 560px)');
 
-// window.addEventListener("load", function() {
-//   body[0].style.overflow = 'hidden';
-//   preloader.style.display = "grid";
-//   setTimeout(() => {
-//     body[0].style.overflow = 'visible';
-//     preloader.style.display = "none";
-//   }, 2000);
-// });
-// function togglePreloader() {
-//   if (mediaQuery1.matches || mediaQuery2.matches || mediaQuery3.matches || mediaQuery4.matches){
-//     body[0].style.overflow = 'hidden';
-//     preloader.style.display = "grid"
-//     setTimeout(() => {
-//       body[0].style.overflow = 'visible';
-//       preloader.style.display = "none";
-//     }, 2000);
-//   }
-//   else{
-//     body[0].style.overflow = 'hidden';
-//     preloader.style.display = "none";
-//   }
-// }
+window.addEventListener("load", function() {
+  body[0].style.overflow = 'hidden';
+  preloader.style.display = "grid";
+  setTimeout(() => {
+    body[0].style.overflow = 'visible';
+    preloader.style.display = "none";
+  }, 2000);
+});
+function togglePreloader() {
+  if (mediaQuery1.matches || mediaQuery2.matches || mediaQuery3.matches || mediaQuery4.matches){
+    body[0].style.overflow = 'hidden';
+    preloader.style.display = "grid"
+    setTimeout(() => {
+      body[0].style.overflow = 'visible';
+      preloader.style.display = "none";
+    }, 2000);
+  }
+  else{
+    body[0].style.overflow = 'hidden';
+    preloader.style.display = "none";
+  }
+}
 
-// mediaQuery1.addListener(togglePreloader);
-// mediaQuery2.addListener(togglePreloader);
-// mediaQuery3.addListener(togglePreloader);
-// mediaQuery4.addListener(togglePreloader);
+mediaQuery1.addListener(togglePreloader);
+mediaQuery2.addListener(togglePreloader);
+mediaQuery3.addListener(togglePreloader);
+mediaQuery4.addListener(togglePreloader);
 
 const form = document.getElementById('contactForm');
 
@@ -140,30 +140,3 @@ form.addEventListener('submit', () => {
     form.querySelector('#message').value = '';
   }, 2000);
 });
-
-// var hasTouchScreen = false;
-
-// if("maxTouchPoints" in navigator)
-//   hasTouchScreen = navigator.maxTouchPoints > 0;
-// else if("msMaxTouchPoints" in navigator)
-//   hasTouchScreen = navigator.msMaxTouchPoints > 0;
-// else{
-//   var mQ = window.matchMedia && matchMedia("(pointer:coarse)");
-
-//   if (mQ && mQ.media === "(pointer:coarse)")
-//     hasTouchScreen = !!mQ.matches;
-//   else if ('orientation' in window)
-//     hasTouchScreen = true; // deprecated, but good fallback
-//   else{
-//     // Only as a last resort, fall back to user agent sniffing
-//     var UA = navigator.userAgent;
-//     hasTouchScreen = (
-//       /\b(BlackBerry|webOS|iPhone|IEMobile)\b/i.test(UA) ||
-//       /\b(Android|Windows Phone|iPad|iPod)\b/i.test(UA)
-//     );
-//   }
-// }
-
-// if (hasTouchScreen){
-//   // Do something here. 
-// }
